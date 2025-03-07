03fe2008a69ac3037d560a6bd472caadca6a2c1c
premmaity <112617129+premmaity@users.noreply.github.com>
2025-03-07 15:05:34 +0530

    Update MultiUserTechHR.py

--- Backend/MultiUserTechHR.py
+++ Backend/MultiUserTechHR.py
@@ -225,4 +225,4 @@ async def get_audio(current_user: dict = Depends(get_current_user)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
